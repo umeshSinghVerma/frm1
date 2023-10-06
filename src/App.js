35c17088f80c0ad0ab937ea80ff3e6f852e98160
Yash7426 <yashagr74260@gmail.com>
2023-10-07 03:08:29 +0530

    created arrays

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  const From = ['alpha','beta','gamma','delta'];
-  const To = ['alpha','beta','gamma','delta'];
+  const [data,setData]=useState([])
+  const [departureArray,setdepartureArray]=useState([])
+  const [arrivalArray,setarrivalArray]=useState([])
+
+  useEffect(()=>{
+    axios
+      .get("http://localhost:4000/CatalogProductOfferingsResponse")
+      .then((res) => {
+        setData(res.data.ReferenceList[0].Flight);
+      })
+      .catch((er) => {
+        console.log("error", er);
+      });
+      let a=[];
+      let b=[];
+    data.forEach((i) => {
+          if (!a.includes(i.Departure.location)) {
+            a.push(i.Departure.location);
+          }
+          if (!b.includes(i.Arrival.location)) {
+            b.push(i.Arrival.location);
+          }
+        });
+        setarrivalArray(b)
+        setdepartureArray(a)
+  },[])
   return (
     <div className="App" style={{display:"flex",gap:"10px",margin:"20px"}}>
       <select name="From" id="From">
         {
-          From.map((item,key)=>{
+          departureArray.map((item,key)=>{
             return <option value={item} key={key}>{item}</option>
           })
         }
       </select>
       <select name="To" id="From">
         {
-          To.map((item,key)=>{
+          arrivalArray.map((item,key)=>{
             return <option value={item} key={key}>{item}</option>
           })
         }
