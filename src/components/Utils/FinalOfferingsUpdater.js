e9a217ea731a6963f0d10cdf6d496281ce6481dd
monarxxjain <jainmonark1000@gmail.com>
2023-12-03 18:56:17 +0530

    Changes :: Comments

--- src/components/Utils/FinalOfferingsUpdater.js
+++ src/components/Utils/FinalOfferingsUpdater.js
@@ -1,75 +1,130 @@
+// Function: FinalOfferingsUpdater
+// Parameters:
+// - displayArray: Array of items to be filtered based on various criteria
+// - brand: Array of selected brand IDs for filtering
+// - flightNo: Array of selected flight numbers for filtering
+// - alldata: Object containing reference data, including flight information
+// - minPrice: Reference to the minimum price input field
+// - maxPrice: Reference to the maximum price input field
+// - setFinalArray: Function to update the state with the filtered array
+// - flight: Array of selected flight carriers for filtering
 function  FinalOfferingsUpdater(displayArray, brand, flightNo, alldata, minPrice, maxPrice, setFinalArray, flight) {
+  // Initialize an array to store the filtered items
     let filteredArr = [];
+
+    // Iterate over each item in the displayArray
     displayArray.map((item) => {
+      // Check if brand filter is applied
       if (brand.length !== 0) {
+        // Check if the item's brand is included in the selected brand IDs
         if (brand.includes(item.ProductBrandOffering[0].Brand?.BrandRef)) {
+          // Check if flight number filter is applied
           if (flightNo.length > 0) {
+            // Determine the effective flight count (limited to 3)
             let x = item.flightRefs.length;
             if (x >= 3) {
               x = 3;
             }
+
+            // Check if the effective flight count is included in the selected flight numbers
             if (flightNo.includes(x)) {
+              // Add the item to the filtered array
               filteredArr.push(item);
             }
           } else {
+            // If no flight number filter, add the item to the filtered array
             filteredArr.push(item);
           }
         }
       } 
-      
+
+      // If brand filter is not applied
       else if (brand.length === 0) {
+        // Check if flight number filter is applied
         if (flightNo.length !== 0) {
+          // Determine the effective flight count (limited to 3)
           let x = item.flightRefs.length;
           if (x >= 3) {
             x = 3;
           }
+
+          // Check if the effective flight count is included in the selected flight numbers
           if (flightNo.includes(x)) {
+            // Add the item to the filtered array
             filteredArr.push(item);
           }
         } else {
+          // If no brand or flight number filter, copy the entire display array
           filteredArr = [...displayArray];
         }
       }
     });
     
+
+    // Check if flight carrier filter is applied  
     if (flight.length) {
+       // Initialize a new array to store items that match the selected flight carriers
       let newarr = [];
+
+      // Iterate over each item in the filtered array
       filteredArr.filter((item) => {
+        // Iterate over each flight reference in the item
         for (let i = 0; i < item.flightRefs.length; i++) {
           let carrier;
+
+          // Find the carrier associated with the current flight reference in the reference data
           for (let j = 0; j < alldata.ReferenceList[0].Flight.length; j++) {
             if (item.flightRefs[i] == alldata.ReferenceList[0].Flight[j].id) {
               carrier = alldata.ReferenceList[0].Flight[j].carrier;
             }
           }
+
+          // Check if the carrier is included in the selected flight carriers
           if (flight.includes(carrier)) {
+             // Add the item to the new array if not already included
             if (!newarr.includes(item)) {
               newarr.push(item);
             }
           }
         }
       });
+      // Update the filtered array with items matching the selected flight carriers
       filteredArr = [...newarr];
     }
     
+
+    // Check if both minPrice and maxPrice filters are applied
     if (minPrice.current.value != "" && maxPrice.current.value != "") {
+
+      // Initialize a new array to store items with prices within the specified range
       let newarr = [];
+      // Iterate over each item in the filtered array
       filteredArr.map((item) => {
+         // Filter the ProductBrandOffering array based on the price range
         let alpha = item.ProductBrandOffering.filter(
           (y) =>
             y.BestCombinablePrice.TotalPrice >= minPrice.current.value &&
             y.BestCombinablePrice.TotalPrice <= maxPrice.current.value
         );
+
+        // Create a deep copy of the item
         let beta = JSON.stringify(item);
         beta = JSON.parse(beta);
+
+        // Update the ProductBrandOffering property with the filtered array
         beta.ProductBrandOffering = alpha;
+
+        // If the filtered array is not empty, add the item to the new array
         if (alpha.length !== 0) {
           newarr.push(beta);
         }
       });
+
+      // Update the filtered array with items within the specified price range
       filteredArr = [...newarr];
     }
 
+    // Update the state with the final filtered array
     setFinalArray(filteredArr);
 }
 
--- src/components/Utils/UtilityFunctions.js
+++ src/components/Utils/UtilityFunctions.js
@@ -1,3 +1,5 @@
+// Function: GetPriceSort
+// Returns a comparator function for sorting items based on their total price
 function GetPriceSort() {
   return function (a, b) {
     let firstmini = a.ProductBrandOffering[0].BestCombinablePrice.TotalPrice;
@@ -12,6 +14,9 @@ function GetPriceSort() {
   };
 }
 
+
+// Function: GetTimeSort
+// Returns a comparator function for sorting items based on their departure time
 function GetTimeSort() {
   return function (a, b) {
     if (a.DepartureTime > b.DepartureTime) {
@@ -23,16 +28,25 @@ function GetTimeSort() {
   };
 }
 
+
+// Function: OfferingConnector
+// Connects and filters data based on departure and arrival locations, sets state variables
 function OfferingConnector(alldata, departureFrom, arrivalTo, setDestinationFlights, setReturnFlights, setAllBrands, setAllFlights) {
+  // Iterate through each CatalogProductOffering in the alldata
   alldata.CatalogProductOfferings.CatalogProductOffering.forEach((item) => {
+    // Check if the offering matches the departure and arrival locations
     if (departureFrom === item.Departure && arrivalTo === item.Arrival) {
+      // Add the offering to the destination flights state variable
       setDestinationFlights((prev) => [...prev, item]);
     }
+    // Check if the offering matches the reversed departure and arrival locations
     if (departureFrom === item.Arrival && arrivalTo === item.Departure) {
+      // Add the offering to the return flights state variable
       setReturnFlights((prev) => [...prev, item]);
     }
   });
 
+  // Extract unique brand information and update the state
   let uniqueBrands = [];
   alldata.ReferenceList[3].Brand.forEach((item) => {
     if (
@@ -46,6 +60,8 @@ function OfferingConnector(alldata, departureFrom, arrivalTo, setDestinationFlig
   });
   setAllBrands(uniqueBrands);
 
+
+  // Extract unique flight information and update the state
   let uniqueFlights = [];
   alldata.ReferenceList[0].Flight.forEach((item) => {
     if (item.carrier && !uniqueFlights.includes(item.carrier)) {
@@ -55,20 +71,29 @@ function OfferingConnector(alldata, departureFrom, arrivalTo, setDestinationFlig
   setAllFlights(uniqueFlights);
 }
 
+
+// Function: DestinationFlight
+// Processes destination flights, extracts relevant information, and updates state variables
 function DestinationFlight(destinationFlights, alldata, setFinalArray, setDisplayArray) {
   destinationFlights.map((item) => {
     let ffr;
     let lfr;
+
+     // Iterate through each ProductBrandOption in the destination flight
     item.ProductBrandOptions.map((f, k) => {
+      // Retrieve flight information for the first and last flight reference
       ffr = alldata.ReferenceList[0].Flight.filter((it) => {
         return it.id === f.flightRefs[0];
       });
       lfr = alldata.ReferenceList[0].Flight.filter((it) => {
         return it.id === f.flightRefs[f.flightRefs.length - 1];
       });
+
+      // Update departure and arrival times in the ProductBrandOption
       f.DepartureTime = ffr[0].Departure.date;
       f.ArrivalTime = lfr[0].Arrival.date;
 
+      // Create a modified copy of the ProductBrandOption with only one ProductBrandOffering
       let alp = f.ProductBrandOffering.map((it) => {
         let g = JSON.stringify(f);
         g = JSON.parse(g);
@@ -76,6 +101,8 @@ function DestinationFlight(destinationFlights, alldata, setFinalArray, setDispla
         g.ProductBrandOffering.push(it);
         return g;
       });
+
+      // Update the final and display arrays with the modified ProductBrandOptions
       setFinalArray((prev) => {
         return [...prev, ...alp];
       });
@@ -86,20 +113,28 @@ function DestinationFlight(destinationFlights, alldata, setFinalArray, setDispla
   });
 }
 
+// Function: ReturnFlight
+// Processes return flights, extracts relevant information, and updates state variable
 function ReturnFlight(returnFlights, alldata, setFinalArrayDup) {
   returnFlights.map((item) => {
     let ffr;
     let lfr;
+
+    // Iterate through each ProductBrandOption in the return flight
     item.ProductBrandOptions.map((f, k) => {
+      // Retrieve flight information for the first and last flight reference
       ffr = alldata.ReferenceList[0].Flight.filter((it) => {
         return it.id === f.flightRefs[0];
       });
       lfr = alldata.ReferenceList[0].Flight.filter((it) => {
         return it.id === f.flightRefs[f.flightRefs.length - 1];
       });
+
+      // Update departure and arrival times in the ProductBrandOption 
       f.DepartureTime = ffr[0].Departure.date;
       f.ArrivalTime = lfr[0].Arrival.date;
 
+      // Create a modified copy of the ProductBrandOption with only one ProductBrandOffering
       let alp = f.ProductBrandOffering.map((it) => {
         let g = JSON.stringify(f);
         g = JSON.parse(g);
@@ -107,6 +142,8 @@ function ReturnFlight(returnFlights, alldata, setFinalArrayDup) {
         g.ProductBrandOffering.push(it);
         return g;
       });
+
+      // Update the final array with the modified ProductBrandOptions
       setFinalArrayDup((prev) => {
         return [...prev, ...alp];
       });
