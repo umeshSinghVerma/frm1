8861477e0b284fc488b841df9db13a240a8cd38a
monarxxjain <jainmonark1000@gmail.com>
2023-12-10 13:09:18 +0530

    Multi City Search made responsive

--- src/components/MultiCitySearch.jsx
+++ src/components/MultiCitySearch.jsx
@@ -63,13 +63,14 @@ const MultiCitySearch = () => {
   };
 
   return (
-    <div className="App rounded-2xl absolute top-[50%] translate-y-[-50%] left-[50%] translate-x-[-50%] bg-white w-fit mx-auto p-20 flex flex-col gap-2 ">
+    <div className="rounded-2xl md:absolute xl:top-[50%] xl:translate-y-[-50%] xl:left-[50%] xl:translate-x-[-50%] bg-white w-full xl:mx-auto p-10 lg:p-20 flex flex-col gap-2 ">
       <p className="text-3xl self-start mb-4">Multi City Search :-</p>
-      <form onSubmit={submit} className="flex flex-col gap-2">
+      <form onSubmit={submit} className="flex flex-col gap-y-10 2xl:gap-y-5">
         {formFields.map((form, index) => {
           return (
-            <div key={index} className="flex gap-10 justify-center">
-              <select name="Departure" onChange={(event) => handleFormChange(event, index)} value={form.Departure} className="w-40 px-2 py-1 rounded-md bg-slate-200">
+            // flex flex-wrap lg:flex-nowrap gap-x-10 gap-y-4 lg:gap-10 lg:justify-center
+            <div key={index} className="grid grid-cols-2 md:grid-cols-5 gap-2">
+              <select name="Departure" onChange={(event) => handleFormChange(event, index)} value={form.Departure} className="px-2 flex-1 py-1 rounded-md bg-slate-200 min-w-[130px]">
                 {departureArray.map((item) => {
                   return (
                     <option key={item} value={item}>
@@ -78,7 +79,7 @@ const MultiCitySearch = () => {
                   );
                 })}
               </select>
-              <select name="Arrival" onChange={(event) => handleFormChange(event, index)} value={form.Arrival} className="w-40 px-2 py-1 rounded-md bg-slate-200">
+              <select name="Arrival" onChange={(event) => handleFormChange(event, index)} value={form.Arrival} className="px-2 flex-1 py-1 rounded-md bg-slate-200 min-w-[130px]">
                 {arrivalArray.map((item) => {
                   return (
                     <option key={item} value={item}>
@@ -87,9 +88,9 @@ const MultiCitySearch = () => {
                   );
                 })}
               </select>
-              <input type="date" name="startDate" placeholder="startDate" onChange={(event) => handleFormChange(event, index)} value={form.startDate} className="bg-slate-200 rounded-md px-2"/>
-              <input type="date" name="endDate" placeholder="endDate" onChange={(event) => handleFormChange(event, index)} value={form.endDate} className="bg-slate-200 rounded-md px-2"/>
-              <button className="border border-red-500 rounded-md px-5 hover:text-white hover:bg-red-500 transition-all" onClick={() => removeFields(index)}>Remove</button>
+              <input type="date" name="startDate" placeholder="startDate" onChange={(event) => handleFormChange(event, index)} value={form.startDate} className=" min-w-[130px] bg-slate-200 rounded-md px-2"/>
+              <input type="date" name="endDate" placeholder="endDate" onChange={(event) => handleFormChange(event, index)} value={form.endDate} className="bg-slate-200 rounded-md px-2 min-w-[130px]"/>
+              <button className="border border-red-500 rounded-md px-5 hover:text-white hover:bg-red-500 transition-all min-w-[130px]" onClick={() => removeFields(index)}>Remove</button>
             </div>
           );
         })}
